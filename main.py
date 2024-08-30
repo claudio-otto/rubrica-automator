9f2ef65ab54d161382beaa546514c1231e1990ad
Claudio Otto <claudio.hans.otto@gmail.com>
2024-08-30 12:06:14 -0400

    Cleans the tmp folder to prevent using data from previous executions

--- main.py
+++ main.py
@@ -1,22 +1,40 @@
+import argparse
+import shutil
+import unicodedata
 import zipfile
 import os
 import openpyxl
+from pathlib import Path
+
+def remove_accents(input_str):
+    nfkd_form = unicodedata.normalize('NFKD', input_str)
+    return "".join([c for c in nfkd_form if not unicodedata.combining(c)])
 
 def obtener_nombres_alumnos(zip_file_path):
     with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
         extraction_path = 'extracted_files'
+
+        # Eliminar el directorio si ya existe
+        if os.path.exists(extraction_path):
+            shutil.rmtree(extraction_path)
+
+        # Crear el directorio nuevamente para extraer los archivos
+        os.makedirs(extraction_path)
+
         zip_ref.extractall(extraction_path)
-        
         extracted_files = os.listdir(extraction_path)
         nombres_alumnos = [nombre.split('_')[0] for nombre in extracted_files]
+        nombres_alumnos.sort(key=remove_accents)
         return nombres_alumnos
 
-def copiar_hoja_excel(archivo_excel, hoja_origen, archivo_zip, archivo_salida):
+
+def copiar_hoja_excel(ruta, archivo_excel, hoja_origen, archivo_zip):
+    path = Path(ruta)
     # Obtener la lista de alumnos desde el archivo ZIP
-    alumnos = obtener_nombres_alumnos(archivo_zip)
+    alumnos = obtener_nombres_alumnos(path / archivo_zip)
 
     # Cargar el archivo original de Excel
-    wb_original_v2 = openpyxl.load_workbook(archivo_excel)
+    wb_original_v2 = openpyxl.load_workbook(path / archivo_excel)
     ws_original_v2 = wb_original_v2[hoja_origen]
 
     # Crear una copia de la hoja original para cada alumno dentro del mismo archivo
@@ -25,12 +43,19 @@ def copiar_hoja_excel(archivo_excel, hoja_origen, archivo_zip, archivo_salida):
         ws_nueva_v2.title = alumno[:31]  # Asegurarse de que el nombre no exceda 31 caracteres
 
     # Guardar el archivo con todas las copias
-    wb_original_v2.save(archivo_salida)
+    wb_original_v2.save(path / "evaluaciones.xlsx")
+
 
 if __name__ == "__main__":
-    archivo_excel = 'Rubrica Evaluacion Final Precio Propiedades (1).xlsx'
-    hoja_origen = 'Hoja1'
-    archivo_zip = 'BOTIC-SOFOF-23-30-13-0006-M3 - EVALUACIÓN FINAL DEL MÓDULO-52388.zip'
-    archivo_salida = 'Final_Rubrica_Alumnos_v3.xlsx'
-    
-    copiar_hoja_excel(archivo_excel, hoja_origen, archivo_zip, archivo_salida)
+    parser = argparse.ArgumentParser(description="Automatiza la creación de hojas de Excel personalizadas.")
+    parser.add_argument('--ruta', type=str, required=True,
+                        help="Ruta con los archivos a leer.")
+    parser.add_argument('--zip', type=str, required=True,
+                        help="Nombre del archivo ZIP que contiene los nombres de los estudiantes.")
+    parser.add_argument('--rubrica', type=str, required=True,
+                        help="Nombre del archivo Excel que contiene la hoja a copiar.")
+    parser.add_argument('--hoja', type=str, required=True, help="Nombre de la hoja que será copiada.")
+
+    args = parser.parse_args()
+
+    copiar_hoja_excel(args.ruta, args.rubrica, args.hoja, args.zip)
